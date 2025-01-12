60ec16c2700b56ff49daf1103377020dd3910c6f
Chris Hunt <chris.hunt@hudl.com>
2025-01-12 15:26:03 +0000

    feat: add more paypal transaction types

--- cli/lib/paypal/consts.ts
+++ cli/lib/paypal/consts.ts
@@ -12,9 +12,9 @@ export const DEFAULT_PAGE_SIZE = 500;
 // Event codes https://developer.paypal.com/docs/transaction-search/transaction-event-codes/
 export const normalTransType = ["T0003", "T0005", "T0006", "T0007"];
 export const cardTransType = ["T0001"];
-export const feeTransType = ["T0106"]; // Chargeback and chargeback processing fee
+export const feeTransType = ["T0106", "T1108"]; // Chargeback and chargeback processing fee, T1108 - fee reversal
 export const virtualTerminalTransType = ["T0012"];
-export const refundTransType = ["T1107", "T1201"]; // Refund and chargeback
+export const refundTransType = ["T1107", "T1201", "T1202"]; // Refund and chargeback
 export const withdrawalTransType = ["T0400", "T0403"];
 
 export const allKnownTransTypes = [
